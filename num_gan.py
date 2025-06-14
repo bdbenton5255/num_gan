9397d8f9c1ba1fb358e971df00da6fe73d124d17
Brandon Benton <brandon.benton.5255@gmail.com>
2025-06-14 06:13:36 -0500

    Examine training data as plot

--- num_gan.py
+++ num_gan.py
@@ -16,3 +16,7 @@ train_labels = torch.zeros(train_data_length)
 train_set = [
     (train_data[i], train_labels[i]) for i in range(train_data_length)
 ]
+
+#Examine training data
+plt.plot(train_data[:, 0], train_data[:, 1], ".")
+plt.show()

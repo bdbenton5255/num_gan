be06c69767a52988b4ca15f2868bce3b56b7a7e9
Brandon Benton <brandon.benton.5255@gmail.com>
2025-06-14 09:10:23 -0500

    Plot training data samples

--- num_gan.py
+++ num_gan.py
@@ -32,3 +32,11 @@ batch_size = 32
 train_loader = torch.utils.data.DataLoader(
     train_set, batch_size=batch_size, shuffle=True
 )
+
+#Plot training data samples
+real_samples, mnist_labels = next(iter(train_loader))
+for i in range(16):
+    ax = plt.subplot(4, 4, i + 1)
+    plt.imshow(real_samples[i].reshape(28, 28), cmap="gray_r")
+    plt.xticks([])
+    plt.yticks([])

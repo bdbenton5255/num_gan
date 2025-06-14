43b86a305de15b6cc368bfc70d631c8e77593aa1
Brandon Benton <brandon.benton.5255@gmail.com>
2025-06-14 09:04:56 -0500

    Preparing, loading training data and creating data loader

--- num_gan.py
+++ num_gan.py
@@ -16,3 +16,19 @@ if torch.cuda.is_available():
 else:
     device = torch.device("cpu")
     print("Using CPU")
+
+#Preaparing training data
+transform = transforms.Compose(
+    [transforms.ToTensor(), transforms.Normalize((0.5), (0.5))]
+)
+
+#Loading the training data
+train_set = torchvision.datasets.MNIST(
+    root=".", train=True, download=True, transform=transform
+)
+
+#Creating the data loader
+batch_size = 32
+train_loader = torch.utils.data.DataLoader(
+    train_set, batch_size=batch_size, shuffle=True
+)

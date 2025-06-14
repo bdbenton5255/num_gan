4b8d108b5730c4c9d39b63493cf9fbeac6b8962b
Brandon Benton <brandon.benton.5255@gmail.com>
2025-06-14 06:43:16 -0500

    Generator network

--- num_gan.py
+++ num_gan.py
@@ -53,3 +53,22 @@ class Discriminator(nn.Module):
 #Instantiate discriminator object
 discriminator = Discriminator()
 
+#Creating the generator network
+class Generator(nn.Module):
+
+    def __init__(self):
+        super().__init__()
+        self.model = nn.Sequential(
+            nn.Linear(2, 16),
+            nn.ReLU(),
+            nn.Linear(16, 32),
+            nn.ReLU(),
+            nn.Linear(32, 2),
+        )
+
+    def forward(self, x):
+        output = self.model(x)
+        return output
+
+#Instantiate generator object
+generator = Generator()

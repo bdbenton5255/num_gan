3b989e7f4350bf105ca8935d4f197500707fe4c9
Brandon Benton <brandon.benton.5255@gmail.com>
2025-06-14 05:59:28 -0500

    Preparing training data

--- num_gan.py
+++ num_gan.py
@@ -3,3 +3,16 @@ from torch import nn
 
 import math
 import matplotlib.pyplot as plt
+
+#Random generator seed for replicability
+torch.manual_seed(111)
+
+#Preparing training data
+train_data_length = 1024
+train_data = torch.zeros((train_data_length, 2))
+train_data[:, 0] = 2 * math.pi * torch.rand(train_data_length)
+train_data[:, 1] = torch.sin(train_data[:, 0])
+train_labels = torch.zeros(train_data_length)
+train_set = [
+    (train_data[i], train_labels[i]) for i in range(train_data_length)
+]

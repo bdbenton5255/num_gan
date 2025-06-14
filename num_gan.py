435c3a44507f09be27a143293847a8183c005a97
Brandon Benton <brandon.benton.5255@gmail.com>
2025-06-14 08:47:42 -0500

    GPU configuration

--- num_gan.py
+++ num_gan.py
@@ -8,3 +8,11 @@ import torchvision.transforms as transforms
 #Random seed generation for experiment
 torch.manual_seed(111)
 
+#GPU configuration
+device = ""
+if torch.cuda.is_available():
+    device = torch.device("cuda")
+    print("Using GPU")
+else:
+    device = torch.device("cpu")
+    print("Using CPU")
